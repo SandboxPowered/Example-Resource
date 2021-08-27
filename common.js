446730fe2a06ba43f02d47c3d30363dcbce2973c
Coded <coded@voxel.games>
2021-08-27 16:08:51 +0100

    Add custom item & block

---
+++ common.js
@@ -0,0 +1,16 @@
+sandbox.on('items', (manager) => {
+    manager.add('test_item', {
+        archetype: 'item' // Optional; defaults to 'item'
+    })
+})
+
+const state = sandbox.getStateProperty('test', 'boolean')
+
+sandbox.on('blocks', (manager) => {
+    manager.add('test_block', {
+        archetype: 'block', // Optional; defaults to 'block'
+        properties: {
+            state: [state] // Optional; defaults to []
+        }
+    })
+})
