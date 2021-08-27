f8ca166d4213bacb1177892e6e6cb9cf6a5ea239
Coded <coded@voxel.games>
2021-08-27 16:23:19 +0100

    Set block material to stone

--- common.js
+++ common.js
@@ -10,6 +10,7 @@ sandbox.on('blocks', (manager) => {
     manager.add('test_block', {
         archetype: 'block', // Optional; defaults to 'block'
         properties: {
+            material: 'stone', // Optional; defaults to 'air'
             state: [state] // Optional; defaults to []
         }
     })
